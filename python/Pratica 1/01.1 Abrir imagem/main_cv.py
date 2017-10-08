04646bedae173c27f54e84c6a5338999c5baea0e
Cássio Santos <sss.cassio@gmail.com>
2017-10-08 12:11:29 -0300

    Add Imread second param comment

--- python/Pratica 1/01.1 Abrir imagem/main_cv.py
+++ python/Pratica 1/01.1 Abrir imagem/main_cv.py
@@ -2,7 +2,7 @@ import cv2
 
 def main() :
     #Le a imagem
-    image = cv2.imread("len_std.png", 1)
+    image = cv2.imread("len_std.png", 1) # cv2.IMREAD_UNCHANGED = -1 / cv2.IMREAD_GRAYSCALE = 0 / cv2.IMREAD_COLOR = 1
     #Mostra a imagem na janela
     cv2.namedWindow("Imagem Original") # define a janela
     cv2.imshow("Imagem Original", image) 
